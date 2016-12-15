026911557d5ec9f110d2e8d7cfc64ee1737e6e55
Romain Demarquez <rssdaa@gmail.com>
2016-12-15 16:57:33 +0100

    changement u gitIgnore, pour enlver le dossier uploads et une route dans le route.php

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,7 @@
 <?php
 	
 	$w_routes = array(
-		['GET',      '/',               'Default#home',    'default_home'],
         ['GET|POST', '/add',            'Pictures#add',    'pictures_add'],
-        ['GET|POST', '/ajax-operation', 'Pictures#geo',    'pictures_ajax-operation']
+        ['GET|POST', '/ajax-operation', 'Pictures#geo',    'pictures_ajax-operation'],
 
 	);
